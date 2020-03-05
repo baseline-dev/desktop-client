b37a1ec2355c9406d9ceae9086817ec7f8d344e5
Nikolai Onken <nikolai@nikolaionken.com>
2020-03-05 12:56:36 +0100

    Fix config loading
    The config loader returned a function and not the config object. This is now fixed.

--- src/util/config.js
+++ src/util/config.js
@@ -1,10 +1,9 @@
 import productionConfig from '../../config/production';
 import developmentConfig from '../../config/development';
 
-async function config() {
-  if (process.env.NODE_ENV === 'production') return productionConfig;
-  return developmentConfig;
-}
+let config;
+if (process.env.NODE_ENV === 'production') config = productionConfig;
+else config = developmentConfig;
 
 export {
   config as default

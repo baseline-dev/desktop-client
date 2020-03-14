9e2a7bb86e5a24650ca4f8be397acfd840fcb810
Nikolai Onken <nikolai@nikolaionken.com>
2020-03-14 19:42:27 +0100

    Added info on how to get support

--- src/util/process.js
+++ src/util/process.js
@@ -1,5 +1,8 @@
 function exit() {
   console.log('\n  👋 Bye friendly human, until next time!\n');
+  console.log('     If you need support or have feedback,');
+  console.log('     you are always welcome to send an email to:\n');
+  console.log('     support@baseline.dev\n');
   process.exit();
 }
 

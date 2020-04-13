e22b7af796c7e5426297fceb2c7df4e8741691ec
Nikolai Onken <nikolai@nikolaionken.com>
2020-04-13 20:53:12 +0200

    Fix spacing.

--- src/template/compiled/user-item.js
+++ src/template/compiled/user-item.js
@@ -3,7 +3,7 @@ var __t, __p = '', __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 __p += '<div class="bg-white overflow-hidden sm:rounded-lg sm:shadow sm:mb-4 sm:border-b sm:border-gray-200">\n    <div class="px-4 py-5 border-b border-gray-200 sm:px-6">\n        <div class="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-no-wrap">\n            <div class="ml-4 mt-4">\n                <div class="flex items-center">\n                    <div class="flex-shrink-0">\n                        <img class="h-12 w-12 rounded-full bg-gray-200" src="' +
 ((__t = ( data.user.avatar )) == null ? '' : __t) +
-'" alt="" />\n                    </div>\n                    <div class="ml-4">\n                        <h3 class="text-lg leading-6 font-medium text-gray-900">\n                            ' +
+'" alt="" />\n                    </div>\n                    <div class="ml-4">\n                        <h3 class="text-lg mb-0 leading-6 font-medium text-gray-900">\n                            ' +
 ((__t = ( data.user.displayName )) == null ? '' : __t) +
 '\n                        </h3>\n                        <p class="text-sm leading-5 text-gray-500">\n                            <a href="#">\n                                ' +
 ((__t = ( data.user.email )) == null ? '' : __t) +
